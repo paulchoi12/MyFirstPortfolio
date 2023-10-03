4a8887f8b4d5695f5db2114e2d2f37eab00cc607
Melmow <paulchoi12@naver.com>
2023-10-03 11:49:24 +0100

    handling 404 error on app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const {controller}= require("./controllers/topic.controllers")
+const {getAllTopics}= require("./controllers/topic.controllers")
+const { handle404Errors, handle500Errors } = require('./controllers/err.controler')
 const app = express()
 
 
-app.get('/api/topics', controller)
+app.get('/api/topics', getAllTopics)
+
+//not found
+app.all('/*', (req, res, next)=>{
+    res.status(404).send({message: "path not found!!"})
+})
+
+//error handling middleware
+app.use(handle500Errors);
 
 module.exports = app
